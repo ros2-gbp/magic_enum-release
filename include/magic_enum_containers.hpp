74b869a39b835ea4c5584c04c1d98b9a723e56c8
Tim Clephas <tim.clephas@nobleo.nl>
2025-12-04 13:54:08 +0100

    Rebase from 'upstream'

--- include/magic_enum_containers.hpp
+++
@@ -1,4 +0,0 @@
-#ifndef MAGIC_ENUM_CONTAINERS_LEGACY_ROOT_HEADER
-#define MAGIC_ENUM_CONTAINERS_LEGACY_ROOT_HEADER
-#include <magic_enum/magic_enum_containers.hpp>
-#endif
--- include/magic_enum_flags.hpp
+++
@@ -1,4 +0,0 @@
-#ifndef MAGIC_ENUM_FLAGS_LEGACY_ROOT_HEADER
-#define MAGIC_ENUM_FLAGS_LEGACY_ROOT_HEADER
-#include <magic_enum/magic_enum_flags.hpp>
-#endif
