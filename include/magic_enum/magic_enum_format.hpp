d0c297da3f89deec4a3e8820ae1625d6c38ca057
Tim Clephas <tim.clephas@nobleo.nl>
2026-08-11 10:24:33 +0200

    Rebase from 'debian/kilted/magic_enum'

--- include/magic_enum/magic_enum_format.hpp
+++ include/magic_enum/magic_enum_format.hpp
@@ -5,7 +5,7 @@
 // | |  | | (_| | (_| | | (__  | |____| | | | |_| | | | | | | | |____|_|   |_|
 // |_|  |_|\__,_|\__, |_|\___| |______|_| |_|\__,_|_| |_| |_|  \_____|
 //                __/ | https://github.com/Neargye/magic_enum
-//               |___/  version 0.9.7
+//               |___/  version 0.9.8
 //
 // Licensed under the MIT License <http://opensource.org/licenses/MIT>.
 // SPDX-License-Identifier: MIT
@@ -58,6 +58,10 @@ std::string format_as(E e) {
 } // namespace magic_enum::format
 
 #if defined(__cpp_lib_format)
+
+#ifndef MAGIC_ENUM_USE_STD_MODULE
+#include <format>
+#endif
 
 template <typename E>
 struct std::formatter<E, std::enable_if_t<std::is_enum_v<std::decay_t<E>>, char>> : std::formatter<std::string_view, char> {
