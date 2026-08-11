322c9f05d4a1538be92d13d51919120532eeecda
Tim Clephas <tim.clephas@nobleo.nl>
2026-08-11 10:31:17 +0200

    Rebase from 'debian/lyrical/magic_enum'

---
+++ test/magic_enum_wchar_t_test_config.hpp
@@ -0,0 +1,14 @@
+// Licensed under the MIT License <http://opensource.org/licenses/MIT>.
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2019 - 2026 Daniil Goncharov <neargye@gmail.com>.
+
+#pragma once
+
+#include <string>
+#include <string_view>
+
+// Configuration header for the wchar_t module test build.
+// Redefines string types to their wide-character equivalents.
+// Used via MAGIC_ENUM_CONFIG_FILE to avoid CMake semicolon-escaping in compile definitions.
+#define MAGIC_ENUM_USING_ALIAS_STRING_VIEW using string_view = std::wstring_view;
+#define MAGIC_ENUM_USING_ALIAS_STRING using string = std::wstring;
